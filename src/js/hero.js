13ddea23f786a8182892bebd903dd2d3fd89d242
Oksana Dziuba <dziuba131303@gmail.com>
2023-06-24 15:02:45 +0400

    edits order now btn

---
+++ src/js/hero.js
@@ -0,0 +1,8 @@
+const orderBtnEl = document.querySelector('.order-btn');
+console.log(orderBtnEl);
+
+orderBtnEl.addEventListener;
+
+orderBtnEl.addEventListener('click', onOrderBtnClick);
+
+function onOrderBtnClick(event) {}
