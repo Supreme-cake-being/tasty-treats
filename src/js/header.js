5000ca1cfc324d839da04604f3e267ad845c1006
MHatalyak <matronahu@gmail.com>
2023-06-28 18:43:52 +0300

    edited header, icons.svg

--- src/js/header.js
+++ src/js/header.js
@@ -8,18 +8,32 @@
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener('click', denyScroll);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', addScroll);
   refs.menuList.addEventListener('click', removeMenu);
+  refs.menuList.addEventListener('click', addScroll);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
   }
 
-  function removeMenu() { 
+  function addScroll() {
+    const scrollY = document.body.style.top;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  }
+
+  function denyScroll() {
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${window.scrollY}px`;
+  }
+
+  function removeMenu() {
     refs.menu.classList.add('is-hidden');
   }
 })();
-
 
 const headerLinks = document.querySelectorAll('.header_link');
 headerLinks.forEach(link => {
