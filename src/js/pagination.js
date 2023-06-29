263cfd67d6cbb0befcbed9f44f6cfb2b91f3c7de
Karr4 but from another universe <121553762+Supreme-cake-being@users.noreply.github.com>
2023-06-29 11:25:41 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,110 +1,113 @@
-import axios from 'axios';
-import Pagination from 'tui-pagination';
-import { createMarkup } from "./create-markup";
+// import axios from 'axios';
+// import Pagination from 'tui-pagination';
+// import { createMarkup } from "./create-markup";
 
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
 
-const fetchRecipes = async (page) => {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}/recipes`, {
-        params: {
-          page: page,
-        }
-      }
-    );
-    const { data } = response;
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+// const containerWidth = document.querySelector('.container');
 
-const containerWidth = document.querySelector('.container');
-const getPaginationSettings = () => {
-  let pageLimit;
-  let visiblePages;
-  switch (containerWidth.clientWidth) {
-    case 1280:
-      pageLimit = 9;
-      visiblePages = 3;
-      break;
+// const getPaginationSettings = () => {
+//   let pageLimit;
+//   let visiblePages;
+//   switch (containerWidth.clientWidth) {
+//     case 1280:
+//       pageLimit = 9;
+//       visiblePages = 3;
+//       break;
     
-    case 768:
-      pageLimit = 8;
-      visiblePages = 3;
-      break;
+//     case 768:
+//       pageLimit = 8;
+//       visiblePages = 3;
+//       break;
     
-    default:
-      pageLimit = 6;
-      visiblePages = 2;
-      break;
-  }
-  return { pageLimit, visiblePages };
-}
-const { pageLimit, visiblePages } = getPaginationSettings();
+//     default:
+//       pageLimit = 6;
+//       visiblePages = 2;
+//       break;
+//   }
+//   return { pageLimit, visiblePages };
+// }
+// const { pageLimit, visiblePages } = getPaginationSettings();
 
-const container = document.querySelector('#tui-pagination-container');
-const options = {
-  totalItems: 0,
-  itemsPerPage: pageLimit,
-  visiblePages: visiblePages,
-  page: 1,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}"></span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}"></span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-}
+// const fetchRecipes = async (page) => {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}/recipes`, {
+//         params: {
+//           page: page,
+//           limit: pageLimit,
+//         }
+//       }
+//     );
+//     const { data } = response;
+//     return data;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 
-const pagination = new Pagination(container, options);
+// const container = document.querySelector('#tui-pagination-container');
+// const options = {
+//   totalItems: 0,
+//   itemsPerPage: pageLimit,
+//   visiblePages: visiblePages,
+//   page: 1,
+//   centerAlign: true,
+//   firstItemClassName: 'tui-first-child',
+//   lastItemClassName: 'tui-last-child',
+//   template: {
+//     page: '<a href="#" class="tui-page-btn tui-page">{{page}}</a>',
+//     currentPage:
+//       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//         '<span class="tui-ico-{{type}}"></span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//         '<span class="tui-ico-{{type}}"></span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//         '<span class="tui-ico-ellip">...</span>' +
+//       '</a>',
+//   },
+// }
 
-const page = pagination.getCurrentPage();
+// const pagination = new Pagination(container, options);
 
-const onRenderPage = async (page) => {
-  try {
-    const response = await fetchRecipes(page);
+// const page = pagination.getCurrentPage();
 
-    if (response.results.length === 0)
-      return container.classList.add('is-hidden');
+// const onRenderPage = async (page) => {
+//   try {
+//     const response = await fetchRecipes(page);
 
-    createMarkup(response.results);
-    container.classList.remove('is-hidden');
+//     if (response.results.length === 0)
+//       return container.classList.add('is-hidden');
 
-    pagination.reset(response.totalPages * pageLimit);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-onRenderPage(page);
+//     createMarkup(response.results);
+//     container.classList.remove('is-hidden');
 
-const createPagination = async (event) => {
-  try {
-    const gallery = document.querySelector('.gallery-list');
-    gallery.innerHTML = '';
+//     pagination.reset(response.totalPages * pageLimit);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+// onRenderPage(page);
 
-    const currentPage = event.page;
+// const createPagination = async (event) => {
+//   try {
+//     const gallery = document.querySelector('.gallery-list');
+//     gallery.innerHTML = '';
 
-    const response = await fetchRecipes(currentPage);
+//     const currentPage = event.page;
 
-    createMarkup(response.results);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+//     const response = await fetchRecipes(currentPage);
 
-pagination.on('afterMove', createPagination);
+//     createMarkup(response.results);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// pagination.on('afterMove', createPagination);
