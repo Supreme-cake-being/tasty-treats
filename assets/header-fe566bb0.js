508f8b6ab91b09a2b321610ebb1408bdfce6282a
Supreme-cake-being <121553762+Supreme-cake-being@users.noreply.github.com>
2023-06-29 17:16:29 +0000

    Deploying to gh-pages from @ Supreme-cake-being/tasty-treats@adbbb5b5a889130b410b852871dab31af54ef0ee 🚀

--- assets/header-fe566bb0.js
+++ assets/header-fe566bb0.js
@@ -12,7 +12,7 @@
             <img class="card-img" src="${r}" />
             <button class="card-favorites-btn" data-id="${n}" type="button">
                 <svg class="icon-heart" width="22" height="22">
-                    <use href="./img/icons.svg#heart"></use>
+                    <use href="/tasty-treats/assets/icons-e30ccde6.svg#heart"></use>
                 </svg>
             </button>
             <div class="card-info">
