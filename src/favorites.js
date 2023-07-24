eab1438af87a55d8c784f10589a8e71df139c50c
supreme-cake-being <XGon20987@gmail.com>
2023-07-24 19:31:02 +0300

    favorites hero issue fix

--- src/favorites.js
+++ src/favorites.js
@@ -142,10 +142,9 @@ function renderPage() {
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
   if (!favorites || !favorites.length) {
     refs.noData.style.display = 'flex';
-    // if (window.innerWidth < 767)
-    //   document.querySelector('.hero-favotites').style.display = 'none';
-    // else
-    //   document.querySelector('.hero-favotites').style.display = 'block';
+    if (window.innerWidth < 767)
+      document.querySelector('.hero-favotites').style.display = 'none';
+    else document.querySelector('.hero-favotites').style.display = 'block';
     document.getElementById('tui-pagination-container').style.display = 'none';
     renderFavotites(favorites);
     renderCategories(null, favorites);
